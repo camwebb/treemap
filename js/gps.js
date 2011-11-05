4ae0fffd078c219359512fe1c83a990cbf1d67ee
Cam Webb <cwebb@oeb.harvard.edu>
2011-11-04 23:41:30 -0400

    map gpd'd

--- js/gps.js
+++ js/gps.js
@@ -12,23 +12,43 @@ function gpsit()
 
 function success_callback(p)
 {
-  var xpx = Math.round(525 + ((+p.coords.longitude.toFixed(3) - -71.115) * 25000)) ;
-  var ypx = Math.round(550 - ((+p.coords.latitude.toFixed(3)  - 42.288) * 25000));  
+  if ( window.location.pathname.substring( window.location.pathname.lastIndexOf('/')+1) == 'map')
+	{
+	  var longOri = queryString('long') ;
+	  var latOri = queryString('lat') ;
+	  var wid = 500 ;
+	  var hig = 500 ;
+	  var factr = 500000 ;
+	  // alert('map!');
+	}
+  else
+	{
+	  var longOri = -71.115 ;
+	  var latOri = 42.288 ;
+	  var wid = 525 ;
+	  var hig = 550 ;
+	  var factr = 25000 ;
+	}
 
-  // alert('lat='+p.coords.latitude.toFixed(3)+';lon='+p.coords.longitude.toFixed(3));
+  var xpx = Math.round( wid + ((+p.coords.longitude.toFixed(3) - longOri ) * factr )) ;
+  var ypx = Math.round( hig - ((+p.coords.latitude.toFixed(3)  - latOri ) * factr));  
   
   // alert('lat=' + ypx + ';lon=' + xpx );
-  //draw(p);
-  var ctx = document.getElementById("gps").getContext("2d");
-  // left of map = -71.136, right of map = -71.115 : width = 
-  var xpx = Math.round(525 + ((+p.coords.longitude.toFixed(3) - -71.115) * 25000)) ;
-  var ypx = Math.round(550 - ((+p.coords.latitude.toFixed(3)  - 42.288) * 25000));
-  // ctx.fillRect(100, 100, 200, 200);
-  var dot = new Image();
-  dot.src = "img/dot.png";
-  dot.onload = function() {
-	ctx.drawImage(dot, xpx, ypx);
-  }
+
+  if ((xpx >= 0) && (xpx < wid) && (ypx >= 0) && (ypx < hig ))
+	{
+	  var ctx = document.getElementById("gps").getContext("2d");
+	  // left of map = -71.136, right of map = -71.115 : width = 
+  
+	  // var xpx = Math.round(525 + ((+p.coords.longitude.toFixed(3) - -71.115) * 25000)) ;
+	  // var ypx = Math.round(550 - ((+p.coords.latitude.toFixed(3)  - 42.288) * 25000));
+
+	  var dot = new Image();
+	  dot.src = "img/dot.png";
+	  dot.onload = function() {
+		ctx.drawImage(dot, xpx, ypx);
+	  }
+	}
 }
 
 function error_callback(p)
@@ -36,6 +56,12 @@ function error_callback(p)
   alert('error='+p.message);
 }               
 
-function  draw(p) {
-}
 
+function queryString( key )
+{
+  // from http://blog.falafel.com/Blogs/AdamAnderson/07-12-17/Parse_a_Query_String_in_JavaScript.aspx
+  var re = new RegExp( "[?&]" + key + "=([^&$]*)", "i" );
+  var offset = location.search.search( re );
+  if ( offset == -1 ) return null;
+  return RegExp.$1;
+}
